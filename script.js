942872af68280f3531f460f153b93dd30a97af9c
afsanaeva <afsanaeva05@gmail.com>
2021-07-10 11:47:52 +0600

    last commit

---
+++ script.js
@@ -0,0 +1,49 @@
+fetch('https://restcountries.eu/rest/v2/all')
+.then(res=> res.json())
+.then(data =>{
+    displayCountries(data);
+})
+
+const displayCountries = countries =>{
+    const countriesDiv = document.getElementById('countries')
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
+
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+
+        // countryDiv.appendChild(h3);
+        // countryDiv.appendChild(p);
+        
+        const countryInfo = `
+        <h3 class = "country-name"> ${country.name} </h3>
+        <p> ${country.capital} </p>
+        <button onclick = "displayCountriesDetail('${country.name}')">Details</button>
+        `;
+        countryDiv.innerHTML =countryInfo;
+        countriesDiv.appendChild(countryDiv);
+        
+    });         
+    }
+
+    const displayCountriesDetail = name =>{
+        const url = `https://restcountries.eu/rest/v2/name/${name}`
+     fetch(url)
+     .then(res=> res.json())
+     .then(data=> renderCountryInfo(data[0]))
+    }
+
+const renderCountryInfo = country =>{
+    const countryDiv = document.getElementById('countryDetail');
+    countryDiv.innerHTML =`
+    <h1>${country.name}</h1>
+    <p>Capital: ${country.capital}</p>
+    <p> Population: ${country.population}</p>
+    <p>Area: ${country.area}</p>
+    <img src ="${country.flag}">
+    `
+}
